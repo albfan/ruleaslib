222372a79050b677ab34d56c022956de205bc55a
Alberto Fanjul <albertofanjul@gmail.com>
2024-03-15 11:24:13 +0100

    without library

--- lib/src/test/java/org/acme/lib/test/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.acme.lib.test;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends RuleTest {
-    // Execute the same tests but in packaged mode.
-}
